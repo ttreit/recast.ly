e327b153c54df5035bb2cf4b922ebd36e39f6622
CF2K <cameronferguson2000@gmail.com>
2020-05-12 18:08:51 -0500

    Complete rendering App component to the DOM

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 // TODO: Render the `App` component to the DOM
 import App from './components/App.js';
+//import myDefault from '/modules/my-module.js';
 
+ReactDOM.render(<App />, document.getElementById("app"));
