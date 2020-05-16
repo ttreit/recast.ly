0642d79456c5716a211f6c514bf0ce3ed9d9191a
CF2K <cameronferguson2000@gmail.com>
2020-05-16 14:10:37 -0500

    Refactor app to be class.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,39 @@ import VideoList from './VideoList.js';
 import exampleVideoData from '../data/exampleVideoData.js';
 import VideoPlayer from './VideoPlayer.js';
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // TODO - Add states.
+    }
+  }
 
+  // TBD
+  render() {
+    return (
+    <div>
+      <nav className="navbar">
+        <div className="col-md-6 offset-md-3">
+          <div><h5><em>search</em> view goes here</h5></div>
+        </div>
+      </nav>
+      <div className="row">
+        <div className="col-md-7">
+          {/* <div><h5><em>videoPlayer</em> view goes here</h5></div> */}
+          <div><VideoPlayer video={exampleVideoData[0]} /></div>
+        </div>
+        <div className="col-md-5">
+          {/* <div><h5><em>videoList</em> view goes here</h5></div> */}
+          <div><VideoList videos={exampleVideoData} /></div>
+        </div>
+      </div>
+    </div>
+  )};
+}
+
+
+/*
 var App = () => (
   <div>
     <nav className="navbar">
@@ -12,17 +44,18 @@ var App = () => (
     </nav>
     <div className="row">
       <div className="col-md-7">
-        {/* <div><h5><em>videoPlayer</em> view goes here</h5></div> */}
+        {/* <div><h5><em>videoPlayer</em> view goes here</h5></div> *//*}
         <div><VideoPlayer video={exampleVideoData[0]} /></div>
       </div>
       <div className="col-md-5">
-        {/* <div><h5><em>videoList</em> view goes here</h5></div> */}
+        {/* <div><h5><em>videoList</em> view goes here</h5></div> *//*}
         <div><VideoList videos={exampleVideoData} /></div>
       </div>
     </div>
   </div>
 );
-
+*/
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 export default App;
+
